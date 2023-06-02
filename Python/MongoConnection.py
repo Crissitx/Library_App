e91d86432363821acb929ad06c133edc4119b3cf
Crissitx <camilocardenas2705@gmail.com>
2023-06-02 17:46:41 -0500

    Update MongoConnection.py
    Now there are scripts for Adding, Updating, Removing and Searching for documents.

--- Python/MongoConnection.py
+++ Python/MongoConnection.py
@@ -8,6 +8,40 @@ def ConnectToMongo():
     except pymongo.errors.ConnectionError as e:
         print("Error connecting to MongoDB:", e)
 
+def AddDocument(client: pymongo.MongoClient, db_name: str, collection_name: str, document):
+    try:
+        # Acceder a la base de datos y la colección
+        db = client[db_name]
+        collection = db[collection_name]
+
+        # Agregar el documento
+        result = collection.insert_one(document)
+
+        print("Documento agregado exitosamente. ID:", result.inserted_id)
+        return True
+
+    except pymongo.errors.CollectionInvalid as e:
+        print("Error al acceder a la colección:", e)
+        return False
+    except pymongo.errors.WriteError as e:
+        print("Error al agregar el documento:", e)
+        return False
+
+def RemoveDocuments(client: pymongo.MongoClient, db_name: str, collection_name: str, filter):
+    try:
+        # Acceder a la base de datos y la colección
+        db = client[db_name]
+        collection = db[collection_name]
+
+        # Eliminar los documentos que coinciden con el filtro
+        result = collection.delete_many(filter)
+
+        print("Documentos eliminados exitosamente. Total:", result.deleted_count)
+    except pymongo.errors.CollectionInvalid as e:
+        print("Error al acceder a la colección:", e)
+    except pymongo.errors.WriteError as e:
+        print("Error al eliminar los documentos:", e)
+
 def SearchDocuments(client: pymongo.MongoClient, db_name: str, collection_name: str, filter):
     try:
         # Acceder a la base de datos y la colección
@@ -55,9 +89,21 @@ def UpdateDocuments(client: pymongo.MongoClient, db_name: str, collection_name: 
         print("Error al actualizar los documentos:", e)
         return False
     
-# TEST
+# UPDATE TEST
 client = ConnectToMongo()
-if client:
-    filter = {"id_area": 1}
-    update = {"DESCRIPCION_AREA": "Literatura"}
-    UpdateDocument(client, "Biblioteca", "area", filter, update)
+
+# UPDATE TEST
+#if client:
+#    filter = {"id_area": 1}
+#    update = {"DESCRIPCION_AREA": "Literatura"}
+#    UpdateDocument(client, "Biblioteca", "area", filter, update)
+
+# ADD TEST
+#if client:
+#    document = {"id_area": 12, "DESCRIPCION_AREA": "Artistica"}
+#    AddDocument(client, "Biblioteca", "area", document)
+
+#REMOVE TEST
+#if client:
+#    filter = {"id_area": 12}
+#    RemoveDocuments(client, "Biblioteca", "area", filter)
