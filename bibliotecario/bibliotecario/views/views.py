3058d5b02b08264fc106465d046f133d8d7e460d
julimejia <davidmejiao43@gmail.com>
2023-06-12 18:24:07 -0500

    pequeño error

--- bibliotecario/bibliotecario/views/views.py
+++ bibliotecario/bibliotecario/views/views.py
@@ -1,59 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from random import randint
 import pymongo
 from subprocess import Popen, PIPE
 import MongoConnection
+from django.shortcuts import redirect
 
-# ELIMINE ESTO Y SOLO IMPORTAMOS DE MONGOCONNECTION
-
-#def AddDocument(client: pymongo.MongoClient, db_name: str, collection_name: str, document):
-#    try:
-#        # Acceder a la base de datos y la colección
-#        db = client[db_name]
-#        collection = db[collection_name]
-#
-#        # Agregar el documento
-#        result = collection.insert_one(document)
-#
-#        print("Documento agregado exitosamente. ID:", result.inserted_id)
-#        return True
-#
-#    except pymongo.errors.CollectionInvalid as e:
-#        print("Error al acceder a la colección:", e)
-#        return False
-#    except pymongo.errors.WriteError as e:
-#        print("Error al agregar el documento:", e)
-#        return False
-
-def id_generator():
-    return randint(1, 10000)
-#def ConnectToMongo():
-#    try:
-#        client = pymongo.MongoClient("mongodb+srv://bibliotecario:bibliotecario123@biblioteca0.mglvdip.mongodb.net/?retryWrites=true&w=majority")
-#        print("Succesfully connected to MongoDB")
-#        return client
-#    except pymongo.errors.ConnectionError as e:
-#        print("Error connecting to MongoDB:", e)
-
-# LO MISMO AQUI
-
-#def UpdateDocument(client: pymongo.MongoClient, db_name: str, collection_name: str, filter, new_data):
-#    try:
-#        # Acceder a la base de datos y la colección
-#        db = client[db_name]
-#        collection = db[collection_name]
-#        
-#        collection.update_one(filter, {"$set": new_data})
-#
-#        print("Documento actualizado exitosamente.")
-#        return True
-#    except pymongo.errors.CollectionInvalid as e:
-#        print("Error al acceder a la colección:", e)
-#        return False
-#    except pymongo.errors.WriteError as e:
-#        print("Error al actualizar el documento:", e)
-#        return False
     
 def index(request):
     return HttpResponse("¡Hola, mundo!")
@@ -108,7 +59,7 @@ def registro(request):
             "edad": request.POST.get('edad')
         }
         MongoConnection.AddDocument(conexion, "Biblioteca", "estudiantes", document)
-        return render(request, 'login.html')
+        return redirect(login())
     return render(request, 'register.html')
 
 def contact(request):
